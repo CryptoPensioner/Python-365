4907c7baa192dc43460dc14a18a8a449afa37edd
Crypto Pensioner <jozwiakdawid@gmail.com>
2023-06-02 21:37:57 +0200

    Create 001.py

---
+++ Tasks/001.py
@@ -0,0 +1,16 @@
+# That should be simple enough....
+
+# Here's a variable with my name
+myName = "CryptoPensioner"
+
+# Here's a simple print() with text and myName variable
+print("Hello, my name is " + myName + ".")
+
+# Formatter print
+print("This is a formatted print: {0}.".format(myName))
+
+# This is an f-string print
+print(f"This is an f-string print: {myName}.")
+
+
+# Cool examples on StackOverflow: https://stackoverflow.com/a/57150615/19921904
