74e734288e05429adad7831aac09cb8f25eaa37f
Crypto Pensioner <jozwiakdawid@gmail.com>
2023-06-03 12:27:45 +0200

    Create 002.py

---
+++ Tasks/002.py
@@ -0,0 +1,9 @@
+# Task 2: Write a Python program that declares variables for your name, age, and favorite color. Print out the values of these variables on separate lines.
+
+name = "CryptoPensioner"
+age = 99
+favorite_color = "black"
+
+print(f"My name: ",name)
+print(f"Age: ",age)
+print(f"My favourite color is: ",favorite_color)
