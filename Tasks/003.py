f261ac894832692e9bfd43ab46f4c0d650286834
Crypto Pensioner <jozwiakdawid@gmail.com>
2023-06-04 17:24:02 +0200

    Create 003.py

---
+++ Tasks/003.py
@@ -0,0 +1,8 @@
+# Task 3: Write a Python program that calculates and prints the area of a rectangle. The length and width of the rectangle should be stored in variables. Use appropriate variable names and print a descriptive message with the calculated area.
+
+length = 10
+width = 5
+area = length * width
+print("The area of the rectangle is", area)
+
+# So far super simple....
